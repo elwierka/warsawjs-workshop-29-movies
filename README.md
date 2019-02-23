3ee28b9c037a05217bf5dbad03fbbe5b067ce892
Anna Guz <elwierka_84@o2.pl>
2019-02-23 13:50:52 +0100

    app-start files

--- README.md
+++
@@ -1,2 +0,0 @@
-# warsawjs-workshop-29-movies
-Stwórz własną bibliotekę filmów krok po kroku
---
+++ app-start/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatIconModule } from '@angular/material/icon';
+import { MatCardModule } from '@angular/material/card';
+
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatIconModule,
+    MatCardModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
